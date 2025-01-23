bbe34631a34e108dd41fef20625b074eea675ffc
Rajjya Rohan Paudyal <84804921+rajjyarohan10@users.noreply.github.com>
2025-01-23 10:26:36 -0500

    Setup Heroku

--- main.py
+++ main.py
@@ -77,4 +77,5 @@ def generate():
 
 # Run the app
 if __name__ == "__main__":
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
